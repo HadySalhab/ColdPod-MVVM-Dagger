3b0b9c54ba91b1bec58aca7dff4645be1c343ff1
Hady Salhab <hadysalhab1@gmail.com>
2020-02-16 01:06:54 +1100

    feat: setup NavigationDrawer

--- app/src/main/java/com/android/myapplication/coldpod/MainActivity.java
+++ app/src/main/java/com/android/myapplication/coldpod/MainActivity.java
@@ -1,14 +1,80 @@
 package com.android.myapplication.coldpod;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.android.myapplication.coldpod.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mBinding;
+    DrawerLayout drawerLayout;
+    NavController navController;
+    AppBarConfiguration mAppBarConfiguration;
+    Toolbar mToolbar;
+    SpannableString mSpannableString;
+    ForegroundColorSpan mForegroundColorSpan;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setupSpannableTitle();
+
+        setupToolbarAndNavDrawer();
+    }
+
+    private void setupSpannableTitle() {
+        mSpannableString = new SpannableString(getString(R.string.app_name));
+        mForegroundColorSpan = new ForegroundColorSpan(ContextCompat.getColor(this,(R.color.secondary_color)));
+        mSpannableString.setSpan(mForegroundColorSpan,4,7, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+    }
+
+    private void setupToolbarAndNavDrawer() {
+        mToolbar = mBinding.toolbar;
+        this.setSupportActionBar(mToolbar);
+        ((AppCompatActivity)this).getSupportActionBar().setTitle(mSpannableString);
+
+        drawerLayout = mBinding.drawerLayout;
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setDrawerLayout(drawerLayout).build();
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getId() == controller.getGraph().getStartDestination()) {
+                    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+                } else {
+                    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+                }
+            }
+        });
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(mBinding.navView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController,mAppBarConfiguration) | super.onSupportNavigateUp();
     }
 }
