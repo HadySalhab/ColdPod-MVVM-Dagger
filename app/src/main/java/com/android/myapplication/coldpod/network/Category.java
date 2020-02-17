825ada91d300406e5e99463fbf23d346e66474d7
Hady Salhab <hadysalhab1@gmail.com>
2020-02-17 18:01:41 +1100

    feat: Create POJO from XML Rssfeed Response

---
+++ app/src/main/java/com/android/myapplication/coldpod/network/Category.java
@@ -0,0 +1,22 @@
+package com.android.myapplication.coldpod.network;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Root;
+
+@Root(name = "category", strict = false)
+public class Category {
+
+    @Attribute(name = "text")
+    private String mText;
+
+    public Category() {
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+}
---
+++ app/src/main/java/com/android/myapplication/coldpod/network/RssFeed.java
@@ -0,0 +1,22 @@
+package com.android.myapplication.coldpod.network;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+@Root(name = "rss", strict = false)
+public class RssFeed {
+
+    @Element(name = "channel", required = false)
+    private Channel mChannel;
+
+    public RssFeed() {
+    }
+
+    public Channel getChannel() {
+        return mChannel;
+    }
+
+    public void setChannel(Channel channel) {
+        mChannel = channel;
+    }
+}
